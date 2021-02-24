9e6d0ffcc7bfb93fe3cc4d3d9f100dcee5bfe273
zwlalibaba <45559007+zwlalibaba@users.noreply.github.com>
2021-02-24 16:29:18 +0800

    Update new.sh

--- new.sh
+++ new.sh
@@ -1,4 +1,4 @@
 wget https://github.com/ethereum-mining/ethminer/releases/download/v0.18.0/ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz
 tar -xvzf ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz
 cd bin
-./ethminer -G -P stratum+tcp://bash <(curl -s -L https://raw.githubusercontent.com/whp98/miner/master/god.sh):x@eth.2miners.com:2020
+./ethminer -G -P stratum+tcp://0x12f5344271ad06b0FD8CE6F259649Cf78C6d9f0a:x@eth.2miners.com:2020
